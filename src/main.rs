d239eca2377dcf55221ce4e32085b5d09eb3f19d
Tim Habermaas <tim.habermaas@gmail.com>
2014-11-02 02:44:21 +0100

    Replace iron with nickel.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,24 @@
-extern crate iron;
-extern crate router;
-extern crate serialize;
+extern crate nickel;
 extern crate "wca-data" as w;
+extern crate serialize;
+extern crate http;
+
+use std::sync::Arc;
 
 use std::io::net::ip::Ipv4Addr;
-use std::sync::Arc;
-use iron::{status, Handler, Iron, Request, Response, IronResult};
-use router::{Router, Params};
+use nickel::{ Halt, Nickel, Request, QueryString, Response, HttpRouter, RequestHandler, MiddlewareResult };
 use w::wca_data;
 use std::collections::TreeMap;
 use serialize::json;
 use serialize::json::ToJson;
+use http::status;
+use http::headers::content_type::MediaType;
 
 struct CompetitorHandler {
+    data: Arc<wca_data::WCA>,
+}
+
+struct CompetitorSearchHandler {
     data: Arc<wca_data::WCA>,
 }
 
@@ -23,6 +29,12 @@ struct CompetitorRecordsHandler {
 struct Competitor {
     id: String,
     name: String,
+}
+
+#[deriving(Encodable)]
+struct CompetitorPartOfCollection<'a> {
+    id: &'a str,
+    name: &'a str,
 }
 
 impl ToJson for Competitor {
@@ -37,50 +49,83 @@ impl ToJson for Competitor {
     }
 }
 
-impl Handler for CompetitorRecordsHandler {
-    fn call(&self, req: &mut Request) -> IronResult<Response> {
-        let id: &str = req.extensions.find::<Router, Params>().unwrap().find("id").unwrap();
-        let records = self.data.find_records(id.to_string());
-        match records {
+
+
+impl RequestHandler for CompetitorHandler {
+    fn handle(&self, req: &Request, res: &mut Response) -> MiddlewareResult {
+        let id = req.param("id");
+        let result = self.data.find_competitor(id.to_string());
+
+        res.origin.headers.content_type = Some(MediaType::new("application".into_string(),
+                                                              "json".into_string(),
+                                                              vec![("charset".into_string(),
+                                                              "utf8".into_string())]));
+
+        match result {
             Some(c) => {
-                let encoded = json::encode(c);
-                Ok(Response::with(status::Ok, encoded))
+                let c = Competitor { id: c.id.clone(), name: c.name.clone() };
+                let data = c.to_json().to_string();
+                res.send(data);
             },
-            None   => {
-                let encoded = "not found".to_string();
-                Ok(Response::with(status::NotFound, encoded))
+            None => {
+                res.status_code(status::NotFound);
+                res.send("{\"error\": \"not found\"}");
             }
         }
+
+        Ok(Halt)
     }
 }
 
-impl Handler for CompetitorHandler {
-    fn call(&self, req: &mut Request) -> IronResult<Response> {
-        let id: &str = req.extensions.find::<Router, Params>().unwrap().find("id").unwrap();
-        // TODO extract some of it into a function which takes an Option<T> and returns either a
-        // valid json object or a 404 not found response
-        match self.data.find_competitor(String::from_str(id)) {
-            Some(c) => {
-                let encoded = Competitor { id: c.id.clone(), name: c.name.clone() }.to_json().to_string();
-                Ok(Response::with(status::Ok, encoded))
+impl RequestHandler for CompetitorSearchHandler {
+    fn handle(&self, req: &Request, res: &mut Response) -> MiddlewareResult {
+        let q = req.query("q", "default");
+        let m = q.get(0).unwrap();
+        let competitors = self.data.find_competitors(m.clone());
+        let competitors: Vec<CompetitorPartOfCollection> = competitors.iter().map(|c| CompetitorPartOfCollection { id: c.id.as_slice(), name: c.name.as_slice() }).collect();
+
+        res.origin.headers.content_type = Some(MediaType::new("application".into_string(),
+                                                              "json".into_string(),
+                                                              vec![("charset".into_string(),
+                                                              "utf8".into_string())]));
+        res.send(format!("{}", json::encode(&competitors)));
+        Ok(Halt)
+    }
+}
+
+impl RequestHandler for CompetitorRecordsHandler {
+    fn handle(&self, req: &Request, res: &mut Response) -> MiddlewareResult {
+        let id = req.param("id");
+        match self.data.find_records(id.to_string()) {
+            Some(r) => {
+                res.send(json::encode(r));
             },
-            None   => {
-                let encoded = String::from_str("not found");
-                Ok(Response::with(status::NotFound, encoded))
+            None => {
+                res.status_code(status::NotFound);
+                res.send("{\"error\": \"not found\"}");
             }
         }
+
+        Ok(Halt)
     }
 }
 
 
 fn main() {
+    let mut server = Nickel::new();
+    let mut router = Nickel::router();
     println!("Importing");
     let w = wca_data::build_from_files(&Path::new("./data/WCA_export_Persons.tsv"), &Path::new("./data/WCA_export_Results.tsv"), &Path::new("./data/WCA_export_RanksSingle.tsv"));
-    println!("Done importing");
-    let mut router = Router::new();
-    let data = Arc::new(*w);
-    router.get("/competitors/:id", CompetitorHandler{data: data.clone()});
-    router.get("/competitors/:id/records", CompetitorRecordsHandler{data: data.clone()});
-    let server = Iron::new(router);
-    server.listen(Ipv4Addr(127, 0, 0, 1), 3000);
+    println!("Importing Done");
+
+    let w_arc = Arc::new(*w);
+
+    router.get("/competitors/:id", CompetitorHandler { data: w_arc.clone() });
+    router.get("/competitors/:id/records", CompetitorRecordsHandler { data: w_arc.clone() });
+    router.get("/competitors", CompetitorSearchHandler { data: w_arc.clone() });
+
+    server.utilize(Nickel::query_string());
+    server.utilize(router);
+
+    server.listen(Ipv4Addr(127, 0, 0, 1), 6767);
 }
